b9466f6f5ae104c6089b88165e92e5a6535ea66b
Kim-kanghyun <61617997+Kim-kanghyun@users.noreply.github.com>
2022-05-08 18:04:37 +0900

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,4 +1,4 @@
 #include"car.h"
 #include<stdio.h>
-void listProduct(Car *slist, int count); // 여러 제품을 리스트 하는 함수
+void listInfo(Car *slist, int count); // 여러 제품을 리스트 하는 함수
 int selectDataNum(Car *slist, int count);  //제품 번호를 고르는 함수
