4a2452b2137790934334ca94dd189389dd594f5a
Kim-kanghyun <61617997+Kim-kanghyun@users.noreply.github.com>
2022-05-08 17:38:16 +0900

    Create car.c

---
+++ car.c
@@ -0,0 +1,43 @@
+#include "car.h"
+
+int selectMenu(){
+int menu;
+ printf("\n*** 렌터카 서비스 ***\n");
+ printf("1. 조회\n");
+ printf("2. 추가\n");
+ printf("3. 수정\n");
+ printf("4. 삭제\n");
+ printf("0. 종료\n\n");
+ printf("=> 원하는 메뉴는? ");
+ scanf("%d", &menu);
+return menu;
+}
+
+int createInfo(Product *s1){
+    printf("사용자 이름은? ");
+    scanf("%s",s1->productName);
+    printf("차 이름은? ");
+    scanf("%s",s1->weight);
+    printf("가격은? ");
+    scanf("%d",&s1->price);
+    printf("대여유형은? (왕복:1, 편도:0)");
+    printf("=> 추가됨!");
+    return 1;
+}
+
+
+int updateProduct(Product *s1){
+    printf("사용자 이름은? ");
+    scanf("%s",s1->productName);
+    printf("차 이름은? ");
+    scanf("%s",s1->weight);
+    printf("가격은? ");
+    scanf("%d",&s1->price);
+    printf("대여유형은? (왕복:1, 편도:0)");
+    printf("=> 수정됨!");
+    return 1;
+}
+
+
+
+
