e07818d9ad4eec05db731308ad85bf91189d87e6
Kim-kanghyun <61617997+Kim-kanghyun@users.noreply.github.com>
2022-05-08 19:35:02 +0900

    Update car.h

--- car.h
+++ car.h
@@ -9,7 +9,6 @@ char location[20];
 int rentalTime;
 int price;
 int rentalType;
-int method;
 } Car;
 
 int selectMenu(); // 메뉴생성 함수
@@ -17,10 +16,10 @@ int createInfo(Car *slist);
 int updateInfo(Car *slist);
 int deleteInfo(Car *slist, int count);
 void readInfo(Car *slist);
-void saveData(Car *slist, int count); //제품내용을 파일로 저장하는 함수
-int loadData(Car *slist); // 파일에 저장되있던 내용을 로드하는 함수
+void saveData(Car *slist, int count); 
+int loadData(Car *slist); 
 void searchByName (Car *slist, int count);
 void searchByTime (Car *slist, int count);
 void searchByModel (Car *slist, int count);
 void calculateTime();
-void calculatePrice(Car *slist, int count);
+void calculatePrice(Car *slist);
